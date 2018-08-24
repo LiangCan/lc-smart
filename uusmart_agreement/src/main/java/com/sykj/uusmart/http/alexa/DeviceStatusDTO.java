55dc6d88198dd16f4482acd0e30827248e64721d
jack <386740421@qq.com>
2018-08-24 16:47:26 +0800

    1. 完善定时的发送消息机制

---
+++ uusmart_agreement/src/main/java/com/sykj/uusmart/http/alexa/DeviceStatusDTO.java
@@ -0,0 +1,22 @@
+package com.sykj.uusmart.http.alexa;
+
+public class DeviceStatusDTO {
+    private String connectivity = "OK";
+    private String S;
+
+    public String getConnectivity() {
+        return connectivity;
+    }
+
+    public void setConnectivity(String connectivity) {
+        this.connectivity = connectivity;
+    }
+
+    public String getS() {
+        return S;
+    }
+
+    public void setS(String s) {
+        S = s;
+    }
+}
